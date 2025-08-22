9bafe181d4398f9b333dfa8dd4602b7f18a30675
DebHtm <yolm9yolm@gmail.com>
2025-08-22 23:51:13 +0300

    DOM3.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,29 @@
 const inputs = document.querySelectorAll('form input');
 
 function fixForm(inputsForm) {
-  inputsForm.forEach((input) => {
+  inputsForm.forEach((input, index) => {
+    if (!input.name) {
+      return;
+    }
+
     const lable = document.createElement('label');
-    const str = input.name;
+
+    if (!input.id) {
+      const safe = input.name
+        .trim()
+        .replace(/\s+/g, '-')
+        .replace(/[^-\w]/g, '');
+
+      input.id = input.name ? `input-${safe}` : `input-${index}`;
+    }
 
     lable.classList.add('field-label');
     lable.setAttribute('for', input.id);
-    lable.textContent = str.toUpperCase();
-    input.before(lable);
-    input.placeholder = lable.textContent[0] + str.slice(1);
+
+    lable.textContent =
+      input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    input.parentNode.appendChild(lable);
+    input.placeholder = lable.textContent;
   });
 }
 
