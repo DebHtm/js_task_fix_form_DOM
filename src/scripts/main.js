b881e88f353b5a83ce4553d46b35857a8ca2b4e3
DebHtm <yolm9yolm@gmail.com>
2025-08-22 23:19:43 +0300

    DOM3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('form input');
+
+function fixForm(inputsForm) {
+  inputsForm.forEach((input) => {
+    const lable = document.createElement('label');
+    const str = input.name;
+
+    lable.classList.add('field-label');
+    lable.setAttribute('for', input.id);
+    lable.textContent = str.toUpperCase();
+    input.before(lable);
+    input.placeholder = lable.textContent[0] + str.slice(1);
+  });
+}
+
+fixForm(inputs);
